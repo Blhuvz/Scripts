a605d5dc6b9b57b1cd8abc064ecd048a428c62a0
Blhuvz <loganmarshall1000@yahoo.com>
2023-12-01 19:42:36 +0000

    Fivem Script
    Script to control the weather

---
+++ FiveM/Weather/cl_weather.lua
@@ -0,0 +1,48 @@
+RegisterCommand("setweather", function(source, args, rawCommand)
+    local weatherType = args[1] or "clear"
+
+    local correctWeatherTypes = {
+        "clear",
+        "extra sunny",
+        "clouds",
+        "overcast",
+        "rain",
+        "thunder",
+        "smog",
+        "foggy",
+        "light rain",
+        "clearing",
+        "neutral",
+        "hazy",
+        "dark",
+        "foggy",
+        "light rain",
+        "xmas",
+        "snowlight",
+        "blizzard"
+    }
+
+    if not table.contains(correctWeatherTypes, weatherType) then
+        TriggerEvent('chat:addMessage', {
+            color = {255, 0, 0},
+            args = {"Invalid weather type. "}
+        })
+        return
+    else
+        SetWeatherTypeNowPersist(weatherType)
+
+        TriggerEvent('chat:addMessage', {
+            color = {52, 128, 235},
+            args = {"The weather is now " .. weatherType}
+        })
+    end
+end, false)
+
+function table.contains(tbl, value)
+    for _, v in ipairs(tbl) do
+        if v == value then
+            return true
+        end
+    end
+    return false
+end
---
+++ FiveM/Weather/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54   'yes'
+
+author 'Blhuvz'
+description 'Small Fivem script to control the weather based off user commands'
+version '1.0.0'
+
+client_scripts {
+  'cl_weather.lua'
+}
