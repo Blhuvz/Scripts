97a68617ad0ab6a8dddddf01db4d7dcdb1145f08
Blhuvz <loganmarshall1000@yahoo.com>
2024-05-10 16:52:16 +0100

    Code

--- JavaScript/clothing_proj.js
+++ JavaScript/clothing_proj.js
@@ -1,13 +1,14 @@
 const database = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Database");
 const terminal = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Terminal");
-const databaseRange = "A2:C";
 const ui = SpreadsheetApp.getUi();
 const terminalRange = terminal.getRange("B6:D6");
+
+const databaseNames = database.getRange("A3:A21").getValues();
 
 function addClothing() {
   if (terminalRange.getValues()[0][0] != "" || terminalRange.getValues()[0][1] != "" || terminalRange.getValues()[0][2] != "") {
     var [terminalName, terminalPrice, terminalLink] = terminalRange.getValues()[0];
-    var nextRow = getNextAvailableRow(database.getRange(databaseRange));
+    var nextRow = getNextAvailableRow(database.getRange("A2:C"));
 
     database.getRange(nextRow, 1, 1, 3).setValues([[terminalName, terminalPrice, `=HYPERLINK("${terminalLink}","${terminalName}")`]]);
 
@@ -16,6 +17,32 @@ function addClothing() {
 
   else {
     ui.alert("There is no data in the terminal to add to the database \n Please validate this!");
+  }
+}
+
+
+function updateClothing() {
+  let clothing = ui.prompt("Enter the name of the clothing you want to update: ");
+  let userInput = clothing.getResponseText();
+
+  let requestedValue = ui.prompt("Enter the name of the new piece of clothing: ");
+  let requestedInput = requestedValue.getResponseText();
+
+  let clothingExists = false;
+
+  for (let i = 0; i < databaseNames.length; i++) {
+    if (databaseNames[i][0] === userInput && userInput != "" && requestedInput != "") {
+      var cell = database.getRange("A" + (i + 3));
+
+      cell.setValue(requestedInput);
+
+      clothingExists = true;
+      break;
+    }
+  }
+
+  if (!clothingExists) {
+    ui.alert("Invalid Input, Please validate your data");
   }
 }
 
@@ -37,5 +64,6 @@ function getNextAvailableRow(sheetRange) {
 function onOpen() {
   ui.createMenu('🛠️ Clothing Tools')
       .addItem('Add Piece of Clothing', 'addClothing')
+      .addItem('Update a Piece of Clothing', 'updateClothing')
       .addToUi();
 }
